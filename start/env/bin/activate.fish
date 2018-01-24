a41620f94645dad82bc1cbe087e716bb6f396bf5
qwerty432 <dimichkit@ukr.net>
2018-01-24 20:04:15 +0200

    started with sentiment analysis

--- start/env/bin/activate.fish
+++ start/env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kotik/git/RealPython/web2py/start/env"
+set -gx VIRTUAL_ENV "/home/kotik/git/web2py/start/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
